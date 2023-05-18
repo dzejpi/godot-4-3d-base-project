dcf988a678698676e8d703d85515d93c00cbc635
DzejPi <dzejjpii@gmail.com>
2023-05-18 19:39:28 +0200

    New game button properly switches the scene now.

---
+++ scripts/ui/NewGameButton.gd
@@ -0,0 +1,17 @@
+extends TextureButton
+
+
+@onready var transition_overlay_scene = $"../../../Transition/TransitionOverlayScene"
+var is_button_pressed = false
+
+
+func _process(delta):
+	if is_button_pressed:
+		if transition_overlay_scene.time_out > 1:
+			get_tree().change_scene_to_file("res://scenes/game/GameScene.tscn")
+
+
+func _on_pressed():
+	transition_overlay_scene.time_out = 0
+	transition_overlay_scene.is_fading_out = false
+	is_button_pressed = true
