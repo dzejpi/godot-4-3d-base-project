ec51598d6d780e7ac46b84efa116fa1609351a5b
DzejPi <dzejjpii@gmail.com>
2023-05-18 19:17:09 +0200

    Implemented Quit Game button.

---
+++ scripts/ui/QuitGameButton.gd
@@ -0,0 +1,22 @@
+extends TextureButton
+
+@onready var transition_overlay_scene = $"../../../Transition/TransitionOverlayScene"
+var is_button_pressed = false
+var platform = OS.get_name()
+
+
+func _ready():
+	if platform == "HTML5":
+		self.disabled = true
+
+
+func _process(delta):
+	if is_button_pressed:
+		if transition_overlay_scene.time_out > 1:
+			get_tree().quit()
+
+
+func _on_pressed():
+	is_button_pressed = true
+	transition_overlay_scene.time_out = 0
+	transition_overlay_scene.is_fading_out = false
