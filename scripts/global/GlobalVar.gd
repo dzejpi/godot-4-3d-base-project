855f31b3ebabecf845014c68350fb85c65c548e8
DzejPi <dzejjpii@gmail.com>
2023-05-18 17:56:17 +0200

    Added global scene with global variable, added the global variable into autoloader.

---
+++ scripts/global/GlobalVar.gd
@@ -0,0 +1,34 @@
+extends Node
+
+
+@onready var music_node = $MusicPlayer
+@onready var sfx_node = $SfxPlayer
+
+# Necessary to replace null with a proper preload("res://...")
+var music_game_music = null
+
+# Necessary to replace null with a proper preload("res://...")
+var sfx_sound_placeholder = null
+
+
+func play_music():
+	music_node.stream = music_game_music
+	music_node.play()
+
+
+func stop_music():
+	music_node.stop()
+
+
+func play_sound(sfx_name):
+	match(sfx_name):
+		"placeholder":
+			sfx_node.stream = sfx_sound_placeholder
+			sfx_node.play()
+
+
+func stop_sound(sfx_name):
+	match(sfx_name):
+		"placeholder":
+			sfx_node.stream = sfx_sound_placeholder
+			sfx_node.stop()
