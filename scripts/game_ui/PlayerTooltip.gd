388b1b3d36f5023403e06c58f176ae6857c3443b
DzejPi <dzejjpii@gmail.com>
2024-09-03 22:23:47 +0200

    Added a new player tooltip scene and connected it into player scene.

---
+++ scripts/game_ui/PlayerTooltip.gd
@@ -0,0 +1,49 @@
+extends Node2D
+
+
+@onready var tooltip_label = $TooltipLabel
+
+var flashing_speed = 1
+var is_flashing = false
+var is_flashing_up = true
+var current_alpha = 0
+
+
+func _ready():
+	self.hide()
+
+
+func _process(delta):
+	if is_flashing:
+		if is_flashing_up:
+			var current_modulation = self.modulate.a
+			if current_modulation < 1:
+				self.modulate.a += flashing_speed * delta
+			else:
+				self.modulate.a = 1
+				is_flashing_up = false
+		else:
+			var current_modulation = self.modulate.a
+			if current_modulation > 0:
+				self.modulate.a -= flashing_speed * delta
+			else:
+				self.modulate.a = 0
+				is_flashing_up = true
+
+
+func display_tooltip(tooltip_text, tooltip_flashing):
+	tooltip_label.text = tooltip_text
+	
+	if tooltip_flashing:
+		self.modulate.a = 0
+		is_flashing = true
+	else:
+		self.modulate.a = 1
+		is_flashing = false
+	
+	self.show()
+
+
+func dismiss_tooltip():
+	is_flashing = false
+	self.hide()
--- scripts/player/PlayerScene.gd
+++ scripts/player/PlayerScene.gd
@@ -12,6 +12,8 @@ const JUMP_VELOCITY = 4.5
 @onready var game_over_scene = $PlayerUI/GameEnd/GameOverScene
 @onready var game_won_scene = $PlayerUI/GameEnd/GameWonScene
 @onready var typewriter_dialog = $PlayerUI/GameUI/TypewriterDialogScene
+@onready var player_tooltip = $PlayerUI/GameUI/PlayerTooltip
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
