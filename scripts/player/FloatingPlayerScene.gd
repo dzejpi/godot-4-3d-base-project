b1af31ea79b08b586de62ab9b07412951c665241
DzejPi <dzejjpii@gmail.com>
2024-09-09 19:35:58 +0200

    Added a floating player controller and added extra crouch keybind to utilise it fully.

---
+++ scripts/player/FloatingPlayerScene.gd
@@ -0,0 +1,144 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const FLOAT_VELOCITY = 0.05
+
+@onready var player_camera = $PlayerHead/Camera
+@onready var ray_cast = $PlayerHead/Camera/RayCast3D
+
+# UI parts
+@onready var game_pause_scene = $PlayerUI/Pause/GamePauseScene
+@onready var game_over_scene = $PlayerUI/GameEnd/GameOverScene
+@onready var game_won_scene = $PlayerUI/GameEnd/GameWonScene
+@onready var typewriter_dialog = $PlayerUI/GameUI/TypewriterDialogScene
+@onready var player_tooltip = $PlayerUI/GameUI/PlayerTooltip
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var mouse_sensitivity = 0.75
+
+var is_game_paused = false
+var is_game_over = false
+var is_game_won = false
+
+# Constant gravitational pull downwards
+var is_pulled_down = false
+
+var debug = false
+
+
+func _ready():
+	transition_overlay.fade_out()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	is_game_paused = false
+	game_pause_scene.hide()
+	game_over_scene.hide()
+	game_won_scene.hide()
+
+
+func _process(_delta):
+	process_collisions()
+	# Update pause state if the user clicks on Continue
+	if is_game_paused:
+		listen_for_pause_button_change()
+
+
+func _input(event):
+	if !is_game_paused && !is_game_over && !is_game_won:
+		if event is InputEventMouseMotion:
+			rotation_degrees.y -= event.relative.x * mouse_sensitivity / 10
+			player_camera.rotation_degrees.x = clamp(player_camera.rotation_degrees.x - event.relative.y * mouse_sensitivity / 10, -90, 90)
+	
+	if Input.is_action_just_pressed("game_pause"):
+		if !is_game_over && !is_game_won:
+			if is_game_paused:
+				is_game_paused = false
+				game_pause_scene.is_game_paused = is_game_paused
+			else:
+				is_game_paused = true
+				game_pause_scene.is_game_paused = is_game_paused
+		
+		update_pause_state()
+
+
+func _physics_process(delta):
+	# Slow both ascent and descent to 0 
+	velocity.y = lerp(velocity.y, 0.0, delta)
+	
+	# Nonstop pull downwards
+	if is_pulled_down:
+		velocity.y -= (gravity / 256) * delta
+	
+	# Floating upwards
+	if Input.is_action_pressed("move_jump"):
+		velocity.y += FLOAT_VELOCITY
+	
+	# Going downwards
+	if Input.is_action_pressed("move_crouch"):
+		velocity.y += FLOAT_VELOCITY * -1
+	
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		if Input.is_action_pressed("move_sprint"):
+			if !is_game_paused && !is_game_over && !is_game_won:
+				velocity.x = direction.x * SPEED * 2
+				velocity.z = direction.z * SPEED * 2
+			else:
+				velocity.x = direction.x * 0
+				velocity.z = direction.z * 0
+		else:
+			if !is_game_paused && !is_game_over && !is_game_won:
+				velocity.x = direction.x * SPEED
+				velocity.z = direction.z * SPEED
+			else:
+				velocity.x = direction.x * 0
+				velocity.z = direction.z * 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	move_and_slide()
+
+
+func process_collisions():
+	if ray_cast.is_colliding():
+		var collision_object = ray_cast.get_collider().name
+		
+		if debug:
+			print("Player is looking at: " + collision_object + ".")
+	else:
+		if debug:
+			print("Player is looking at: nothing.")
+
+
+func update_pause_state():
+	if is_game_paused:
+		game_pause_scene.show()
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		game_pause_scene.hide()
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func listen_for_pause_button_change():
+	if !(game_pause_scene.is_game_paused):
+		is_game_paused = game_pause_scene.is_game_paused
+		update_pause_state()
+
+
+func toggle_game_over():
+	is_game_over = true
+	game_over_scene.show()
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func toggle_game_won():
+	is_game_won = true
+	game_won_scene.show()
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
