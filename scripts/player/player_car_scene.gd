e48c0d8384657ada8d010900cc7e03fc02c82d47
DzejPi <dzejjpii@gmail.com>
2025-03-24 21:56:28 +0100

    Car scene rewritten to include variable types, returns, inefficient functions rewritten.

--- scripts/player/player_car_scene.gd
+++ scripts/player/player_car_scene.gd
@@ -1,109 +1,89 @@
 extends CharacterBody3D
 
 
-const ACCELERATION = 10.0
-const MAX_SPEED = 180
-const MAX_REVERSE_SPEED = -60
-const TURN_SPEED = 60
-const JUMP_VELOCITY = 4.5
-const BREAKING_SPEED = 10.0
+const ACCELERATION: float = 10.0
+const MAX_SPEED: float = 180.0
+const MAX_REVERSE_SPEED: float = -20
+const TURN_SPEED: float = 60.0
+const JUMP_VELOCITY: float = 4.5
+const BREAKING_SPEED: float = 20.0
+const GRAVITY_MULTIPLIER: float = 10
 
-@onready var player_camera = $PlayerHead/Camera
-@onready var ray_cast = $PlayerHead/Camera/RayCast3D
+@onready var player_camera: Camera3D = $PlayerHead/Camera
+@onready var ray_cast: RayCast3D = $PlayerHead/Camera/RayCast3D
 
 # UI parts
-@onready var game_pause_scene = $PlayerUI/Pause/GamePauseScene
-@onready var game_over_scene = $PlayerUI/GameEnd/GameOverScene
-@onready var game_won_scene = $PlayerUI/GameEnd/GameWonScene
-@onready var typewriter_dialog = $PlayerUI/GameUI/TypewriterDialogScene
-@onready var player_tooltip = $PlayerUI/GameUI/PlayerTooltip
-@onready var speed_label = $PlayerUI/GameUI/SpeedLabel
+@onready var typewriter_dialog: Node2D = $PlayerUI/GameUI/TypewriterDialogScene
+@onready var player_tooltip: Node2D = $PlayerUI/GameUI/PlayerTooltip
+
+@onready var game_over_scene: Node2D = $PlayerUI/GameEnd/GameOverScene
+@onready var game_won_scene: Node2D = $PlayerUI/GameEnd/GameWonScene
+
+@onready var speed_label: Label = $PlayerUI/GameUI/SpeedLabel
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var mouse_sensitivity = 0.75
+var mouse_sensitivity: float = 0.75
 
-var is_game_paused = false
-var is_game_over = false
-var is_game_won = false
+var current_speed: float = 0.0
 
-var current_speed = 0.0
+# Debug for console info
+var debug: bool = true
 
-var debug = false
+# Last collider player looked at
+var last_looked_at: String = ""
 
 
-func _ready():
-	transition_overlay.fade_out()
+func _ready() -> void:
+	GlobalVar.reset_game()
+	TransitionOverlay.fade_out()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	is_game_paused = false
-	game_pause_scene.hide()
-	game_over_scene.hide()
-	game_won_scene.hide()
 
 
-func _process(_delta):
+func _process(_delta: float) -> void:
 	process_collisions()
-	
-	speed_label.text = str(int(current_speed))
-	
-	# Update pause state if the user clicks on Continue
-	if is_game_paused:
-		listen_for_pause_button_change()
 
 
-func _input(event):
-	if !is_game_paused && !is_game_over && !is_game_won:
+func _input(event: InputEvent) -> void:
+	if GlobalVar.is_game_active:
 		if event is InputEventMouseMotion:
 			rotation_degrees.y -= event.relative.x * mouse_sensitivity / 10
 			player_camera.rotation_degrees.x = clamp(player_camera.rotation_degrees.x - event.relative.y * mouse_sensitivity / 10, -90, 90)
-	
-	if Input.is_action_just_pressed("game_pause"):
-		if !is_game_over && !is_game_won:
-			if is_game_paused:
-				is_game_paused = false
-				game_pause_scene.is_game_paused = is_game_paused
-			else:
-				is_game_paused = true
-				game_pause_scene.is_game_paused = is_game_paused
-		
-		update_pause_state()
 
 
-func _physics_process(delta):
-	velocity.x = 0
-	#velocity.z = 0
+func _physics_process(delta: float) -> void:
+	# Not processing at all if the game isn't active
+	if not GlobalVar.is_game_active:
+		return
 	
+	# Acceleration and braking
 	if Input.is_action_pressed("move_up"):
-		if current_speed < MAX_SPEED:
-			current_speed += ACCELERATION * delta
+		current_speed = clamp(current_speed + ACCELERATION * delta, MAX_REVERSE_SPEED, MAX_SPEED)
 	elif Input.is_action_pressed("move_down"):
-		if current_speed > MAX_REVERSE_SPEED:
-			current_speed -= ACCELERATION * 2 * delta
+		current_speed = clamp(current_speed - ACCELERATION * delta / 2, MAX_REVERSE_SPEED, MAX_SPEED)
 	else:
-		if current_speed > 2:
-			current_speed -= BREAKING_SPEED * delta
-		elif current_speed < -2:
-			current_speed += BREAKING_SPEED * delta
-		else:
-			current_speed = 0
+		# Smooth slowdown
+		current_speed = move_toward(current_speed, 0, BREAKING_SPEED * delta)
 	
 	var forward_direction = -transform.basis.z.normalized()
 	velocity = forward_direction * current_speed
 	
+	# Steering
 	if Input.is_action_pressed("move_left"):
-		if current_speed > 2:
+		if current_speed > 0:
 			rotation_degrees.y += TURN_SPEED * delta
-		elif current_speed < -2:
+		elif current_speed < 0:
 			rotation_degrees.y -= TURN_SPEED * delta
 	elif Input.is_action_pressed("move_right"):
-		if current_speed > 2:
+		if current_speed > 0:
 			rotation_degrees.y -= TURN_SPEED * delta
-		elif current_speed < -2:
+		elif current_speed < 0:
 			rotation_degrees.y += TURN_SPEED * delta
 	
+	# Gravity
 	if not is_on_floor():
-		velocity.y -= gravity * 10 * delta
+		velocity.y -= gravity * GRAVITY_MULTIPLIER * delta
 	else:
 		velocity.y = 0
 	
@@ -111,39 +91,25 @@ func _physics_process(delta):
 	move_and_slide()
 
 
-func process_collisions():
+func process_collisions() -> void:
 	if ray_cast.is_colliding():
-		var collision_object = ray_cast.get_collider().name
-		
-		if debug:
-			print("Player is looking at: " + collision_object + ".")
+		var collision_object: String = ray_cast.get_collider().name
+		if collision_object != last_looked_at:
+			last_looked_at = collision_object
+			if debug:
+				print("Player is looking at: " + collision_object + ".")
 	else:
-		if debug:
-			print("Player is looking at: nothing.")
+		if last_looked_at != "nothing":
+			last_looked_at = "nothing"
+			if debug:
+				print("Player is looking at: nothing.")
 
 
-func update_pause_state():
-	if is_game_paused:
-		game_pause_scene.show()
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	else:
-		game_pause_scene.hide()
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+func trigger_game_over() -> void:
+	GlobalVar.toggle_game_over()
+	game_over_scene.show_game_over()
 
 
-func listen_for_pause_button_change():
-	if !(game_pause_scene.is_game_paused):
-		is_game_paused = game_pause_scene.is_game_paused
-		update_pause_state()
-
-
-func toggle_game_over():
-	is_game_over = true
-	game_over_scene.show()
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-
-func toggle_game_won():
-	is_game_won = true
-	game_won_scene.show()
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+func trigger_game_won() -> void:
+	GlobalVar.toggle_game_won()
+	game_won_scene.show_game_won()
