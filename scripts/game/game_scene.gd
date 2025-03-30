5f2c5f25b26614af84d8e20c63ee0e8bc550101b
DzejPi <dzejjpii@gmail.com>
2025-03-30 20:43:19 +0200

    Added a function into the game script that returns the amount of nodes (useful for debugging).

--- scripts/game/game_scene.gd
+++ scripts/game/game_scene.gd
@@ -1,5 +1,19 @@
 extends Node3D
 
 
+@export var debug: bool = false
+@export var child_count_countdown: float = 1
+var current_child_count_countdown: float = child_count_countdown
+
+
 func _ready() -> void:
 	pass
+
+
+func _process(delta: float) -> void:
+	if debug:
+		if current_child_count_countdown > 0:
+			current_child_count_countdown -= delta
+		else:
+			current_child_count_countdown = child_count_countdown
+			print("Game nodes: " + str(get_child_count()))
