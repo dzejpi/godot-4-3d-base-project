5d6843e414d42eb754be5f24b928920aadccc4b7
DzejPi <dzejjpii@gmail.com>
2025-03-31 19:17:21 +0200

    Typewriter script improved, but still needs extra care - variables might be somewhat incorrectly used despite it appearing to work properly.

--- scripts/game_ui/typewriter_dialog_scene.gd
+++ scripts/game_ui/typewriter_dialog_scene.gd
@@ -1,59 +1,55 @@
 extends Node2D
 
 
-@onready var text_label = $TextNode/TextBackgroundSprite/TextLabel
+@onready var text_label: Label = $TextNode/TextBackgroundSprite/TextLabel
 
-var dialog_text = ["Placeholder text."]
-var is_dialog_displayed = false
+var dialog_text: Array[String] = ["Placeholder text."]
+var is_dialog_displayed: bool = false
 
-# How quickly new letter appears in seconds
-var letter_change_speed = 16
+# How quickly dialog disappears in seconds
+const DIALOG_CHANGE_SPEED: float = 16.0
 
-# Timeout for the single letters
-var letter_timeout_current = 0
-var letter_timeout_base_time = 1
+# Timeout for single letters
+const DIALOG_SWITCH_TIMEOUT_BASE_TIME: float = 1.0
 
-# Timeout for another dialog to appear
-var dialog_switch_timeout_current = 0
-var dialog_switch_timeout_base_time = 1
+# Timeout for another letter to appear
+const LETTER_TIMEOUT_BASE_TIME: float = 1.0
+var letter_timeout_current: float = LETTER_TIMEOUT_BASE_TIME
+var dialog_switch_timeout_current: float = DIALOG_SWITCH_TIMEOUT_BASE_TIME
 
 # Which line out of all lines is displayed
-var currently_displayed_dialog_from_array = 0
+var currently_displayed_dialog_index: int = 0
 
 # Which letter from the current line is displayed
-var currently_displayed_letter = 0
+var currently_displayed_letter: int = 0
 
 
-func _ready():
+func _ready() -> void:
 	visible = false
 	is_dialog_displayed = false
 
 
-func _process(delta):
+func _process(delta: float) -> void:
 	if is_dialog_displayed:
 		process_dialog(delta)
-	else:
+	elif visible:
 		visible = false
-	
-	#print("Dialog switch timeout current:" + str(dialog_switch_timeout_current))
 
 
-func start_dialog(dialog_array, delta):
+func start_dialog(dialog_array: Array[String], delta: float) -> void:
 	dialog_text = dialog_array
 	visible = true
 	is_dialog_displayed = true
 	
-	currently_displayed_dialog_from_array = 0
+	currently_displayed_dialog_index = 0
 	currently_displayed_letter = 0
-	
-	process_dialog(delta)
 
 
-func process_dialog(delta):
-	var letters_on_current_line = dialog_text[currently_displayed_dialog_from_array].length()
-	var dialogs_in_array = dialog_text.size() - 1;
+func process_dialog(delta: float) -> void:
+	var letters_on_current_line: int = dialog_text[currently_displayed_dialog_index].length()
+	var dialogs_in_array: int = dialog_text.size() - 1
 	
-	# If the player wants to display the whole character
+	# Player wants to display the whole dialog at once
 	if Input.is_action_just_pressed("dialog_forward"):
 		# Show all letters at once
 		if currently_displayed_letter < letters_on_current_line:
@@ -62,19 +58,20 @@ func process_dialog(delta):
 			adjust_dialog_switch_timeout(letters_on_current_line)
 		else:
 			# All letters are displayed, start showing the next line of the dialog
-			currently_displayed_dialog_from_array += 1
+			currently_displayed_dialog_index += 1
 			# Start displaying letters from the beginning
 			currently_displayed_letter = 0
 			# Hide the dialog in case there are no dialogs left
-			if currently_displayed_dialog_from_array > dialogs_in_array:
+			if currently_displayed_dialog_index > dialogs_in_array:
 				is_dialog_displayed = false
+				return
 	
 	# Automatic behavior
 	if letter_timeout_current > 0:
-		letter_timeout_current -= (letter_change_speed * delta)
+		letter_timeout_current -= DIALOG_CHANGE_SPEED * delta
 	else:
 		# Reset timeout for the letter
-		letter_timeout_current = letter_timeout_base_time
+		letter_timeout_current = LETTER_TIMEOUT_BASE_TIME
 		
 		# Add extra letter
 		if currently_displayed_letter < letters_on_current_line:
@@ -82,26 +79,29 @@ func process_dialog(delta):
 			# If message was just displayed fully, start with the dialog switch timeout
 			if currently_displayed_letter == letters_on_current_line:
 				adjust_dialog_switch_timeout(letters_on_current_line)
+		
 		if currently_displayed_letter == letters_on_current_line:
 			if dialog_switch_timeout_current > 0:
-				dialog_switch_timeout_current -= (letter_change_speed * delta)
+				dialog_switch_timeout_current -= DIALOG_CHANGE_SPEED * delta
 			else:
+				currently_displayed_dialog_index += 1
+				currently_displayed_letter = 0
+				if currently_displayed_dialog_index > dialogs_in_array:
+					is_dialog_displayed = false
+					return
+				
 				# Switch timeout ran out, display new dialog
 				adjust_dialog_switch_timeout(letters_on_current_line)
-				currently_displayed_dialog_from_array += 1
-				currently_displayed_letter = 0
-				if currently_displayed_dialog_from_array > dialogs_in_array:
-					is_dialog_displayed = false
 	
 	# Display the text, if possible
-	if currently_displayed_dialog_from_array > dialogs_in_array:
+	if currently_displayed_dialog_index <= dialogs_in_array:
+		text_label.text = dialog_text[currently_displayed_dialog_index].left(currently_displayed_letter)
+	else:
 		is_dialog_displayed = false
-	else:
-		text_label.text = dialog_text[currently_displayed_dialog_from_array].left(currently_displayed_letter)
 
 
-func adjust_dialog_switch_timeout(letters_on_line):
+func adjust_dialog_switch_timeout(letters_on_line: int) -> void:
 	if letters_on_line <= 30:
-		dialog_switch_timeout_current = dialog_switch_timeout_base_time + (letters_on_line)
+		dialog_switch_timeout_current = DIALOG_SWITCH_TIMEOUT_BASE_TIME + letters_on_line
 	else:
-		dialog_switch_timeout_current = dialog_switch_timeout_base_time + (letters_on_line / 4)
+		dialog_switch_timeout_current = DIALOG_SWITCH_TIMEOUT_BASE_TIME + (letters_on_line / 4.0)
